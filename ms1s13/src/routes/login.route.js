69cc4a6861b659354e02b06dd67aec720f2689d2
Alex Athayde Viana Ribeiro <alex_a_ribeiro@estudante.sesisenai.org.br>
2024-04-23 22:24:44 -0300

    add controllers

--- ms1s13/src/routes/login.route.js
+++ ms1s13/src/routes/login.route.js
@@ -1,7 +1,5 @@
-const { Router, query } = require('express') // 
-const Aluno = require('../models/Aluno')
-
-const { sign } = require('jsonwebtoken')
+const { Router } = require('express')
+const LoginControllers = require('../controllers/LoginController')
 
 const loginRoutes = new Router()
 
@@ -9,37 +7,7 @@ loginRoutes.get('/bem_vindo', (req, res) => {
     res.json({ name: 'Bem vindo' })
 })
 
-loginRoutes.post('/', async (req, res) => {
-    try {
-        const email = req.body.email
-        const password = req.body.password
-
-        if (!email) {
-            return res.status(400).json({ message: 'O email é obrigatório' })
-        }
-
-        if (!password) {
-            return res.status(400).json({ message: 'O password é obrigatório' })
-        }
-
-        const aluno = await Aluno.findOne({
-            where: {email:email, password:password}
-        })
-
-        if(!aluno){
-            return res.status(404).json({ error: 'Nenhum aluno corresponde a email e senha fornecidos!' })
-        }
-
-        const payload = {sub: aluno.id, email: aluno.email, nome: aluno.nome}
-
-        const token = sign(payload, process.env.SECRET_JWT)        
-
-        res.status(200).json({Token: token})
-
-    } catch (error) {
-        return res.status(500).json({ error: error, message: 'Algo deu errado!' })
-    }
-})
+loginRoutes.post('/', LoginControllers.login )
 
 
 
